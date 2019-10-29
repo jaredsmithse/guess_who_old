d98f840df97b0c5b585aaffcf2c9e028af8163aa
Jared Smith <dev@jaredsmith.se>
2019-10-29 15:35:18 -0700

    Added annotations to employee and company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id           :integer          not null, primary key
+#  name         :string           not null
+#  email_domain :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Company < ApplicationRecord
   validates :name, presence: true
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id                     :integer          not null, primary key
+#  name                   :string           not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_employees_on_email                 (email) UNIQUE
+#  index_employees_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
