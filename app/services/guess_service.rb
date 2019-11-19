f306c7e4aaf20f9489cdb1f147cb0c2c2e91e410
Jared Smith <dev@jaredsmith.se>
2019-11-19 17:09:36 +0800

    Updated GuessService to use company scope again

--- app/services/guess_service.rb
+++ app/services/guess_service.rb
@@ -7,7 +7,7 @@ class GuessService
   end
 
   def employees_to_guess
-    @employees_to_guess ||= Employee.
+    @employees_to_guess ||= employees.
       where.not(id: [correct_employee.id, @employee.id]).
       limit(3).
       to_a.
@@ -16,6 +16,10 @@ class GuessService
   end
 
   def correct_employee
-    @correct_employee ||= Employee.where.not(id: @employee.id).order("RANDOM()").first
+    @correct_employee ||= employees.where.not(id: @employee.id).order("RANDOM()").first
+  end
+
+  def employees
+    @employee.company.employees
   end
 end
